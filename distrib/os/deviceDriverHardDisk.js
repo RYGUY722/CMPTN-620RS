4f796b71d515d84345793a11144a689fb1e4c908
Ryan Sheffler <Ryan.Sheffler1@marist.edu>
2020-12-23 22:16:39 -0500

    Working on file system
    Just working on the file system. No real specific changelog this time, just making steady progress. Addresses #21 .

--- distrib/os/deviceDriverHardDisk.js
+++ distrib/os/deviceDriverHardDisk.js
@@ -20,20 +20,120 @@ var TSOS;
             for (let x = 0; x < HDD_TRACKS; x++) {
                 for (let y = 0; y < HDD_SECTORS; y++) {
                     for (let z = 0; z < HDD_BLOCKS; z++) {
-                        sessionStorage.setItem(x + " " + y + " " + z, "00 00 00 00");
+                        sessionStorage.setItem(x + "" + y + "" + z, "00000000");
                     }
                 }
             }
+            sessionStorage.setItem("000", "1100" + ((HDD_TRACKS - 1) * HDD_SECTORS * HDD_BLOCKS)); // I use the MBR to store the remaining data blocks. This way, I can easily reference it when adding a file to check if it will fit.
         }
         create(filename) {
+            if (true) { // Check that there is enough space in the file system
+                if (!this.isTooLarge(filename)) {
+                    var filenamehex = this.translateToASCII(filename);
+                    //generateMessage(1,
+                }
+            }
         }
         write(filename, contents) {
         }
         deleteFile(filename) {
         }
         list(perm) {
+            var mbrSkip = 1; // This variable lets us easily skip over the MBR as an entry in the file directory. Set to 1 at first, set to 0 after the first sector.
+            for (let y = 0; y < HDD_SECTORS; y++) {
+                for (let z = mbrSkip; z < HDD_BLOCKS; z++) {
+                    var dirEntry = sessionStorage.getItem(0 + "" + y + "" + z);
+                    if (this.inUse(dirEntry)) { // If the location is in use, 
+                        if (!(perm == 0 && (dirEntry.charAt(4) + dirEntry.charAt(5)) == "2E")) { // If the permission is set to 0 and the first character is a "." (2E in ASCII hex), then the file shouldn't be shown.
+                            _StdOut.putText(this.translateToPlain(dirEntry.substring(3, dirEntry.length))); // Otherwise, print the filename.
+                        }
+                    }
+                }
+                mbrSkip = 0;
+            }
         }
-        findFreeLocation() {
+        findFile(filename) {
+            var mbrSkip = 1; // This variable lets us easily skip over the MBR as an entry in the file directory. Set to 1 at first, set to 0 after the first sector.
+            var filenamehex = this.translateToASCII(filename); // All data is stored in hex ASCII, so we should translate the filename to that for comparison purposes.
+            for (let y = 0; y < HDD_SECTORS; y++) {
+                for (let z = mbrSkip; z < HDD_BLOCKS; z++) {
+                    var dirEntry = sessionStorage.getItem(0 + "" + y + "" + z);
+                    if (this.inUse(dirEntry)) { // If the location is in use, 
+                        if (dirEntry.substring(3, dirEntry.length) == filenamehex) { // Check the filename. If it's equal to our target,
+                            return 0 + "" + y + "" + z; // Return this key.
+                        }
+                    }
+                }
+                mbrSkip = 0;
+            }
+            return -1; // File not found
+        }
+        findFreeLocation(type) {
+            var mbrLink = this.getLink(sessionStorage.getItem("0 0 0"));
+            if (mbrLink != "000") { // The MBR's link location will sometimes be the 
+                // Reset the MBR's link to 000.
+                return mbrLink; // Return that value
+            }
+            else { // If we don't have a location saved for quick use in the MBR link, we need to find one ourself.
+                if (type == 0) {
+                    for (let y = 0; y < HDD_SECTORS; y++) {
+                        for (let z = 0; z < HDD_BLOCKS; z++) {
+                            if (!this.inUse(sessionStorage.getItem(0 + "" + y + "" + z))) { // If the location isn't in use, return that key.
+                                return 0 + "" + y + "" + z;
+                            }
+                        }
+                    }
+                }
+                else {
+                    for (let x = 1; x < HDD_TRACKS; x++) {
+                        for (let y = 0; y < HDD_SECTORS; y++) {
+                            for (let z = 0; z < HDD_BLOCKS; z++) {
+                                if (!this.inUse(sessionStorage.getItem(x + "" + y + "" + z))) { // If the location isn't in use, return that key.
+                                    return x + "" + y + "" + z;
+                                }
+                            }
+                        }
+                    }
+                }
+                return -1; // We've got to return something.
+            }
+        }
+        translateToASCII(data) {
+            var newdata = "";
+            for (let i = 0; i < data.length; i++) {
+                let chunk = data.charCodeAt(i).toString(16);
+                if (chunk.length < 2) { // As always, we need to make sure the resulting hex value is 2 characters long.
+                    chunk += "0";
+                }
+                newdata += chunk;
+            }
+            return newdata;
+        }
+        translateToPlain(data) {
+            var newdata = "";
+            for (let i = 0; i < data.length; i += 2) {
+                let chunk = (data.charAt(i) + data.charAt(i + 1));
+                chunk = chunk.toString(10);
+                newdata += String.fromCharCode(chunk);
+            }
+            return newdata;
+        }
+        generateMessage(inUse, locationLink, data) {
+            var finData = data;
+            while (finData.length > (HDD_BLOCK_SIZE - 4)) {
+                finData = finData + "0";
+            }
+            var finalMsg = inUse + locationLink + finData;
+            return finalMsg;
+        }
+        inUse(data) {
+            if (data.charAt(0) == '1') {
+                return true;
+            }
+            return false;
+        }
+        getLink(data) {
+            return data.substring(1, 3); // The link is the 2-4 bytes.
         }
         blockSize(data) {
             return Math.floor(data.length / (HDD_BLOCK_SIZE - 4)) + 1; // We add 1 because all data will take up at least one block. 
--- source/os/deviceDriverHardDisk.ts
+++ source/os/deviceDriverHardDisk.ts
@@ -24,14 +24,20 @@ module TSOS {
 			for(let x = 0; x < HDD_TRACKS; x++){
 				for(let y = 0; y < HDD_SECTORS; y++){
 					for(let z = 0; z < HDD_BLOCKS; z++){
-						sessionStorage.setItem(x+" "+y+" "+z, "00 00 00 00");
+						sessionStorage.setItem(x+""+y+""+z, "00000000");
 					}
 				}
 			}
+			sessionStorage.setItem("000", "1100"+((HDD_TRACKS-1)*HDD_SECTORS*HDD_BLOCKS)); // I use the MBR to store the remaining data blocks. This way, I can easily reference it when adding a file to check if it will fit.
 		}
 		
 		public create(filename) { // This sets up a file directory entry with the given filename.
-			
+			if(true) { // Check that there is enough space in the file system
+				if(!this.isTooLarge(filename)){
+					var filenamehex = this.translateToASCII(filename);
+					//generateMessage(1,
+				}
+			}
 		}
 		
 		public write(filename, contents) { // This writes the value of contents to filename.
@@ -43,11 +49,108 @@ module TSOS {
 		}
 		
 		public list(perm){ // Lists the files in the directory. If perm is 0, lists files as normal. If perm is 1, shows hidden files as well.
-			
+			var mbrSkip = 1; // This variable lets us easily skip over the MBR as an entry in the file directory. Set to 1 at first, set to 0 after the first sector.
+			for(let y = 0; y < HDD_SECTORS; y++){
+				for(let z = mbrSkip; z < HDD_BLOCKS; z++){
+					var dirEntry = sessionStorage.getItem(0+""+y+""+z); 
+					if(this.inUse(dirEntry)){ // If the location is in use, 
+						if(!(perm==0 && (dirEntry.charAt(4)+dirEntry.charAt(5))=="2E")){ // If the permission is set to 0 and the first character is a "." (2E in ASCII hex), then the file shouldn't be shown.
+							_StdOut.putText(this.translateToPlain(dirEntry.substring(3,dirEntry.length))); // Otherwise, print the filename.
+						}
+					}
+				}
+				mbrSkip = 0;
+			}
 		}
 		
-		public findFreeLocation(){ // Finds the next free location on the disk.
-			
+		public findFile(filename){ // Iterates through the file directory to find a file with the given name.
+			var mbrSkip = 1; // This variable lets us easily skip over the MBR as an entry in the file directory. Set to 1 at first, set to 0 after the first sector.
+			var filenamehex = this.translateToASCII(filename); // All data is stored in hex ASCII, so we should translate the filename to that for comparison purposes.
+			for(let y = 0; y < HDD_SECTORS; y++){
+				for(let z = mbrSkip; z < HDD_BLOCKS; z++){
+					var dirEntry = sessionStorage.getItem(0+""+y+""+z); 
+					if(this.inUse(dirEntry)){ // If the location is in use, 
+						if(dirEntry.substring(3,dirEntry.length) == filenamehex){ // Check the filename. If it's equal to our target,
+							return 0+""+y+""+z; // Return this key.
+						}
+					}
+				}
+				mbrSkip = 0;
+			}
+			return -1; // File not found
+		}
+		
+		public findFreeLocation(type){ // Finds the next free location on the disk. If type is set to 0, looks for a free space in the first track (the file directory). If set to 1, looks for one anywhere else.
+		var mbrLink = this.getLink(sessionStorage.getItem("0 0 0"));
+			if(mbrLink != "000"){ // The MBR's link location will sometimes be the 
+				// Reset the MBR's link to 000.
+				return mbrLink; // Return that value
+			}
+			else{ // If we don't have a location saved for quick use in the MBR link, we need to find one ourself.
+				if(type==0){
+					for(let y = 0; y < HDD_SECTORS; y++){
+						for(let z = 0; z < HDD_BLOCKS; z++){
+							if(!this.inUse(sessionStorage.getItem(0+""+y+""+z))){ // If the location isn't in use, return that key.
+								return 0+""+y+""+z;
+							}
+						}
+					}
+				}
+				else{
+					for(let x = 1; x < HDD_TRACKS; x++){
+						for(let y = 0; y < HDD_SECTORS; y++){
+							for(let z = 0; z < HDD_BLOCKS; z++){
+								if(!this.inUse(sessionStorage.getItem(x+""+y+""+z))){ // If the location isn't in use, return that key.
+									return x+""+y+""+z;
+								}
+							}
+						}
+					}
+				}
+				return -1; // We've got to return something.
+			}
+		}
+		
+		private translateToASCII(data) { // This method takes plaintext data, translates it to ASCII in hex, then returns the new string.
+			var newdata = "";
+			for(let i = 0; i < data.length; i++){
+				let chunk = data.charCodeAt(i).toString(16);
+				if(chunk.length<2){ // As always, we need to make sure the resulting hex value is 2 characters long.
+					chunk += "0";
+				}
+				newdata += chunk;
+			}
+			return newdata;
+		}
+		
+		private translateToPlain(data) { // This method takes ASCII in hex data, translates it back to plaintext, then returns the new string.
+			var newdata = "";
+			for(let i = 0; i < data.length; i+=2){
+				let chunk = (data.charAt(i)+data.charAt(i+1));
+				chunk = chunk.toString(10);
+				newdata += String.fromCharCode(chunk);
+			}
+			return newdata;
+		}
+		
+		private generateMessage(inUse, locationLink, data) { // This method takes the constituent parts of a memory entry and forms a single string of them, filling the empty space in data with 0's.
+			var finData= data;
+			while(finData.length>(HDD_BLOCK_SIZE-4)){
+				finData=finData+"0";
+			}
+			var finalMsg = inUse+locationLink+finData;
+			return finalMsg;
+		}
+		
+		private inUse(data) { // This method returns if the given data is in use according to the first bit.
+			if(data.charAt(0)=='1'){
+				return true;
+			}
+			return false;
+		}
+		
+		private getLink(data) { // This method returns the linked location from the given data string.
+			return data.substring(1,3); // The link is the 2-4 bytes.
 		}
 		
 		private blockSize(data) { // This method returns the size in blocks of the given data, useful to make sure the file can fit on the disk.
